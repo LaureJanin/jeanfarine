44e9977cdf8ad5d084fc6c0416bdc84eb007581e
LAURE JANIN <ljanin51@gmail.com>
2023-04-03 16:44:21 +0200

    debut de la page histoire de la troupe

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -5,33 +5,79 @@ function History() {
     return (
         <>
             <h1>L'histoire de la troupe</h1>
-                 <section className="timeline">
-                    <div className="container left">
-		                <div className="content">
-			                <h2>Tecnologia</h2>
-			                    <p>
-				                    A tecnologia está em constante evolução, sempre trazendo novas possibilidades e transformando a forma como vivemos e interagimos com o mundo. Com a crescente adoção de dispositivos móveis, a internet se tornou mais acessível e poderosa do que nunca, permitindo que pessoas e empresas se conectem e realizem tarefas de forma mais eficiente.
-			                    </p>
-		                </div>
-	                </div>
+            <section className="timeline">
+                <div className="container left">
+		            <div className="content">
+			            <h2>1997</h2>
+			            <p>Un groupe de parents d'élèves de l'école de Saint Germain Nuelles se réunit avec l'envie de monter une pièce de théâtre et de la jouer dans le cadre des activités de l'association. La pièce choisie - <i>Les Palmes de Monsieur Schutz</i> - se révèle difficile à mettre en oeuvre pour les amateurs que nous sommes.</p>
+                        <p>Les difficultés de la mise en scène rendent notre travail laborieux. Nous réalisons que le métier de metteur en scène ne s'invente pas !</p>
+		            </div>
+	            </div>
 
-	                <div className="container right">
-		                <div className="content">
-			                <h2>Arte</h2>
-			                    <p>
-				                    A arte é uma expressão humana que transcende o tempo e o espaço. Ela é capaz de transmitir emoções e ideias de forma única e pessoal, proporcionando novas perspectivas e reflexões sobre a vida. Desde a antiguidade, a arte tem sido um meio de comunicação e conexão entre as pessoas, e continua a inspirar e encantar as gerações atuais.
-			                    </p>
-		                </div>
-	                </div>
+	            <div className="container right">
+		            <div className="content">
+			            <h2>1999</h2>
+			            <p>Avec l'aide de Michel Bernier, metteur en scène professionnel, la pièce aboutit enfin et se joue pour la première fois à Nuelles.</p>
+                        <p> Michel nous fait partager son talent et l'envie de poursuivre l'aventure s'impose à nous naturellement.</p>
+                        <p>Ainsi de nouvelles pièces sont montées avec le renouvellement d'une partie des membres de la troupe.</p>
+		            </div>
+	            </div>
 
-	                <div className="container left">
-		                <div className="content">
-			                <h2>Matematica</h2>
-			                    <p>
-				                    A matemática é uma disciplina fundamental que está presente em todas as áreas do conhecimento e da vida cotidiana. Ela nos ajuda a entender e resolver problemas complexos, desde a contagem de objetos simples até cálculos avançados de engenharia e ciência. Além disso, a matemática é a base da lógica e do raciocínio, desenvolvendo habilidades críticas e analíticas em seus estudantes. Sem a matemática, muitos avanços tecnológicos e científicos não teriam sido possíveis.
-			                    </p>
-		                </div>
-	                </div>
+	            <div className="container left">
+		            <div className="content">
+			            <h2>1999 - Les Palmes de Monsieur Schutz de Jean-Noël Fenwick</h2>
+			            <p>Mise en scène : Michel Bernier</p>
+                        <p>Son et lumière : Monique Aujogue</p>
+		             </div>
+	            </div>
+
+                <div className="container right">
+		            <div className="content">
+			            <h2>2003 - Un Air de Famille d'Agnès Jaoui et Jean-Pierre Bacri</h2>
+                        <p>Mise en scène : Michel Bernier</p>
+                        <p>Son et lumière : Marie Russel</p>
+                        <p>Octobre 2003 : participation au festival contemporain amateur "Terre de Scènes" au théâtre de Villefranche</p>
+		            </div>
+	            </div>
+
+                <div className="container left">
+		            <div className="content">
+			            <h2>2006 - La Cantatrice Chauve d'Eugène Ionesco</h2>
+			            <p>Mise en scène : Michel Bernier</p>
+                        <p>Son et lumière : Jacques Janin</p>
+                        <p>2007 : la pièce est jouée au cours de notre premier festival de théâtre amateur sur les communes de Nuelles et Saint Germain sur l'Arbresle</p>
+		             </div>
+	            </div>
+
+                <div className="container right">
+		            <div className="content">
+			            <h2>2008 - Les précieuses ridicules de Molière</h2>
+                        <p>Mise en scène : Michel Bernier</p>
+                        <p>Son et lumière : Jacques Janin</p>
+		            </div>
+	            </div>
+
+                <div className="container left">
+		            <div className="content">
+			            <h2>2011 - Echafaudage de Luc Girerd</h2>
+			            <p>Mise en scène : Michel Bernier</p>
+                        <p>Son et lumière : Thomas Dormoy</p>
+		             </div>
+	            </div>
+
+                <div className="container right">
+		            <div className="content">
+			            <h2>2013 - La Noce de Bertolt Brecht</h2>
+                        <p>Mise en scène : Michel Bernier</p>
+		            </div>
+	            </div>
+
+                <div className="container left">
+		            <div className="content">
+			            <h2>2016 - Le jeu de l'amour et du hazard, Marivaux</h2>
+			            <p>Mise en scène : Karine Giraud</p>
+		             </div>
+	            </div>
 
                 </section>
         </>
