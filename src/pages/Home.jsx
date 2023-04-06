99354aa62f306d58d7fa05378aed2abb84ab6f2b
LAURE JANIN <ljanin51@gmail.com>
2023-04-06 12:27:50 +0200

    mise en page de la home avec le responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,21 +11,32 @@ function Home() {
     return (
         <section id="home">
             <div className="text">{letters}</div>
-            <div className="slider"></div>
-            
-            <div className="jeanfarine">
-                <h2>Jean Farine ?</h2>
-                <p>C'est un niais, un benet. 
-                <br />Ce terme populaire vient du théâtre de la farce, où l’acteur joue un imbécile dont la figure enfarinée lui a vallu le nom de "Jean-Farine". 
-                <br /> On le nomme aussi Pierrot.
-                </p>
+            <div className="slider">
+                <div className="jeanfarine">
+                    <h2>JEAN FARINE ?</h2>
+                    <p>C'est un niais, un benet. 
+                    <br />Ce terme populaire vient du théâtre de la farce, où l’acteur joue un imbécile dont la figure enfarinée lui a vallu le nom de "Jean-Farine". 
+                    <br />Il se nomme aussi Pierrot.
+                    </p>
+                </div>
             </div>
- 
-            <div className="actualite"> 
-                <h2>Qui sommes nous ?</h2>
-                <p>La troupe des Jean Farine a été créée en 1997 à Saint Germain sur L'Arbresle. Amateurs passionnés de théâtre, nous jouons différentes pièces tout public dans lesquelles l'humour tient le premier rôle.</p>
-                <h2>Actualités</h2>
-                <p>Notre nouvelle pièce en préparation, Le truc du Tralala, actuellement en répétition, sera joué prochainement. Restez connecté pour en savoir plus. Des dates seront annoncées prochainement</p>
+            <div className="titre">
+                <h2>À venir</h2>
+            </div>
+            <div className="actualite">
+                <div className="box">
+                    <h2>Qui sommes-nous ?</h2>
+                    <p>La troupe des Jean Farine a été créée en 1997 à Saint Germain Nuelles. 
+                    Amateurs passionnés de théâtre, nous jouons différentes pièces tout public dans lesquelles l'humour tient souvent le premier rôle.
+                    </p>
+                </div>
+                <div className="box">
+                    <h2>Actualités</h2>
+                    <p>Notre nouvelle pièce en préparation, <span id="italic">Le truc du Tralala</span>, actuellement en répétition, sera jouée prochainement. 
+                    Des dates seront annoncées prochainement.
+                    Restez connecté pour en savoir plus. 
+                    </p>
+                </div>
             </div> 
         </section>
     )
