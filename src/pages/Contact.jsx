4811cfe1d9c81ab69555e26ca73bc41be8c1ac8a
LAURE JANIN <ljanin51@gmail.com>
2023-04-20 12:41:03 +0200

    message confirmation envoi du formulaire + scss responsive

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,14 @@
+import React, { useState } from "react";
 import Title from "../components/Title/Title";
 import emailjs from '@emailjs/browser';
 import "./styles/contact.scss";
 
 function Contact() {
+    const [name, setName] = useState("");
+    const [userEmail, setUserEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [confirmation, setConfirmation] = useState(false);
+
     const sendEmail = (e) => {
         e.preventDefault();
         emailjs.sendForm(
@@ -13,6 +19,13 @@ function Contact() {
         )
         .then((res) => {
           console.log(res);
+          setName("");
+          setUserEmail("");
+          setMessage("");
+          setConfirmation(true);
+          setTimeout(() => {
+            setConfirmation(false);
+        }, 5000);
         })
         .catch(err => console.log(err));
     };
@@ -20,6 +33,7 @@ function Contact() {
     return (
         <section id="contact">
             <Title text="Nous contacter" />
+            {confirmation && <p>Merci pour votre message, nous revenons vers vous au plus vite !</p>}
             <form onSubmit={sendEmail}>
                 <div className="box1">
                     <label htmlFor="name"></label>
@@ -27,6 +41,8 @@ function Contact() {
                     type="text"
                     name="name"
                     placeholder="Votre nom..."
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                     required
                     />
                 </div>
@@ -36,12 +52,19 @@ function Contact() {
                     type="email"
                     name="user_email"
                     placeholder="Votre adresse mail : exemple@gmail.com"
+                    value={userEmail}
+                    onChange={(e) => setUserEmail(e.target.value)}
                     required
                     />
                 </div>
                 <div className="box3">
                     <label htmlFor="message"></label>
-                    <textarea name="message" placeholder="Message..." />
+                    <textarea 
+                    name="message" 
+                    placeholder="Message..."
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    />
                 </div>
                 <div className="box4">
                     <button
