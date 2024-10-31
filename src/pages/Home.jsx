c88f4297f60c7078ec5698c157fc74965c565978
laure.janin <laure.janin@bedrockstreaming.com>
2024-10-31 15:39:43 +0100

    logo afghanes home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import "./styles/home.scss";
+import logo from "../../public/affiche-soutien-femmes-afghanes_logo.png";
+
 
 function Home() {
   return (
@@ -7,6 +9,7 @@ function Home() {
       <div className="slider">
         <div className="jeanfarine">
           <h2>JEAN FARINE ?</h2>
+
           <p>
             Ce terme populaire vient du théâtre de la farce, où l’acteur joue un
             imbécile dont la figure enfarinée lui a vallu le nom de <br />«
@@ -22,6 +25,7 @@ function Home() {
       <div className="actualite">
         <div className="box">
           <h2>Qui sommes-nous ?</h2>
+
           <p>
             La troupe Les Jean Farine a été créée en 1997 à Saint Germain
             Nuelles par un groupe de parents d'élèves.
@@ -35,19 +39,20 @@ function Home() {
           </p>
         </div>
         <div className="box">
-          <h2>Actualités</h2>
-          <p>
-          L'association Les Jean Farine organise un évènement le <span id="bold">9 novembre 2024</span> pour témoigner de sa solidarité et apporter un soutien financier aux <span id="bold">jeunes filles et femmes afghanes</span>, privées de leurs droits
-          fondamentaux.
-          <br />
-          <br />
-          <span id="bold">Au programme :</span> ateliers créatifs, exposition, chorale, conférence et spectacle.
-          <br />
-          <br />
-            <a href="./afghanes" target="_blank">
-              <span id="italic">Cliquer ici pour en savoir plus</span>
-            </a>
-          </p>
+          <h2>Actualités</h2>            
+            <p>
+            L'association Les Jean Farine organise un évènement le <span id="bold">9 novembre 2024</span> pour témoigner de sa solidarité et apporter un soutien financier aux <span id="bold">jeunes filles et femmes afghanes</span>, privées de leurs droits
+            fondamentaux.
+              <br />
+              <br />
+              <span id="bold">Au programme :</span> ateliers créatifs, exposition, chorale, conférence et spectacle.
+              <br />
+              <br />
+              <a href="./afghanes" target="_blank">
+                <img src={logo} alt="Logo" className="logo" />
+                <span id="italic">Cliquer ici pour en savoir plus</span>
+              </a>
+            </p>
         </div>
       </div>
     </section>
