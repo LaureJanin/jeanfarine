07094332244fd66e4b10cbed9d3a59fbc341d256
laure.janin <laure.janin@bedrockstreaming.com>
2024-11-24 16:57:48 +0100

    remove afghane home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,33 +25,19 @@ function Home() {
       <div className="actualite">
         <div className="box">
           <h2>Qui sommes-nous ?</h2>
-
           <p>
             La troupe Les Jean Farine a été créée en 1997 à Saint Germain
             Nuelles par un groupe de parents d'élèves.
-            <br />
-            <br />
+          </p>
+          <p>
             Amateurs passionnés de théâtre, nous jouons différentes pièces tout
             public dans lesquelles l'humour tient souvent le premier rôle.
-            <br />
-            <br />
-            Restez connecté pour en savoir plus.
           </p>
         </div>
         <div className="box">
           <h2>Actualités</h2>            
             <p>
-            L'association Les Jean Farine organise un évènement le <span id="bold">9 novembre 2024</span> pour témoigner de sa solidarité et apporter un soutien financier aux <span id="bold">jeunes filles et femmes afghanes</span>, privées de leurs droits
-            fondamentaux.
-              <br />
-              <br />
-              <span id="bold">Au programme :</span> ateliers créatifs, exposition, chorale, conférence et spectacle.
-              <br />
-              <br />
-              <a href="./afghanes" target="_blank">
-                <img src={logo} alt="Logo" className="logo" />
-                <span id="italic">Cliquer ici pour en savoir plus</span>
-              </a>
+            Restez connecté, nous revenons très vite avec de nouveaux projets.
             </p>
         </div>
       </div>
